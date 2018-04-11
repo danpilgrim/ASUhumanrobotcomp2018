ea3547da7a01e75d72bc8e73c2e379f89515160a
danpilgrim <d.daniel.pilgrim@gmail.com>
2018-04-10 18:07:40 -0700

    update

--- JShandyman/index.js
+++ JShandyman/index.js
@@ -4,7 +4,6 @@ var ROS = new ROSlib.Ros({
 });
 
 
-
 //console log
 ROS.on('connection', function() {
 console.log('Connected to websocket server.');
@@ -16,53 +15,26 @@ ROS.on('close', function() {
 console.log('Connection to websocket server closed.');
 });
 
-/* NOTES FROM teleop_key_handyman.cpp from Handyman-ROS
-const float linear_coef         = 0.2f;
 
-float move_speed = 1.0f;
-
-moveBase(pub_base_twist, +linear_coef*move_speed, 0.0, 0.0);
-
-ros::Publisher  pub_base_twist = node_handle.advertise<geometry_msgs::Twist> (pub_base_twist_topic_name, 10);
-
-
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
-void HandymanTeleopKey::moveBase(ros::Publisher &publisher, double linear_x, double linear_y, double angular_z)
-{
-  geometry_msgs::Twist twist;
-
-  twist.linear.x  = linear_x;
-  twist.linear.y  = linear_y;
-  twist.linear.z  = 0.0;
-  twist.angular.x = 0.0;
-  twist.angular.y = 0.0;
-  twist.angular.z = angular_z;
-
-  publisher.publish(twist);
-}
-
-*/
 
 // creates our topic
 var cmd_vel = new ROSlib.Topic({
     ros: ROS,
-    name: "/hsrb/opt_command_velocity",          // NOTE: make sure names are correct!
+    name: "/turtle1/cmd_vel",          // NOTE: make sure names are correct!
     messageType: "geometry_msgs/Twist"
 });
-
 
 //creates our message
 var message = new ROSlib.Message({
     linear:{
-        x: 0.20000000298,
-        y: 0,
-        z: 0
+        x: 1,
+        y: 2,
+        z: 3
     },
     angular:{
-        x: 0,
-        y: 0,
-        z: 0
+        x: 1,
+        y: 2,
+        z: 3
     }
 });
 
